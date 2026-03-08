26269831fd1a81d61bfd96543f19a3733ba819bc
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-08 12:56:54 -0600

    2026-03-08 12:55 About to shift the project back to server.markhome.mcf from io.github.msobkow

--- src/main/java/io/github/msobkow/v3_1/cfint/cfintsaxloader/CFIntSaxLoader.java
+++ src/main/java/io/github/msobkow/v3_1/cfint/cfintsaxloader/CFIntSaxLoader.java
@@ -62,14 +62,14 @@ public class CFIntSaxLoader
 {
 
 	// The namespace URI of the supported schema
-	public final static String	SCHEMA_XMLNS = "http://msobkow.github.io/msobkow/xsd/v3_1/cfint-structured.xsd";
+	public final static String	SCHEMA_XMLNS = "https://msobkow.github.io/msobkow/v3_1/xsd/cfint-structured.xsd";
 
 	// The source for loading the supported schema
-	public final static String	SCHEMA_URI = "/opt/msobkow/3.0.1/xsd/cfint-structured.xsd";
-	public final static String	SCHEMA_ROOT_URI = "/xsd/cfint-structured.xsd";
-	public final static String CFSEC_XMLNS = "http://msobkow.github.io/msobkow/xsd/v3_1/cfsec-structured.xsd";
-	public final static String CFSEC_URI = "/opt/msobkow/3.0.1/xsd/cfsec-structured.xsd";
-	public final static String CFSEC_ROOT_URI = "/xsd/cfsec-structured.xsd";
+	public final static String	SCHEMA_URI = "https://msobkow.github.io/msobkow/v3_1/xsd/cfint-structured.xsd";
+	public final static String	SCHEMA_ROOT_URI = "https://msobkow.github.io/msobkow/v3_1/xsd/cfint-structured.xsd";
+	public final static String CFSEC_XMLNS = "https://msobkow.github.io/msobkow/v3_1/xsd/cfsec-structured.xsd";
+	public final static String CFSEC_URI = "https://msobkow.github.io/msobkow/xsd/v3_1/cfsec-structured.xsd";
+	public final static String CFSEC_ROOT_URI = "https://msobkow.github.io/msobkow/xsd/v3_1/cfsec-structured.xsd";
 
 
 	// The schema instance to load in to
