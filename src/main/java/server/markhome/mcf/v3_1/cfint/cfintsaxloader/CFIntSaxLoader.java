0ed82b63fa5b103fa72c404626d53ac088281b9d
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-08 23:06:18 -0600

    2026-03-08 23:05 CST Cleaned up more of the details of the migration back to mcf.markhome.server/server.markhome.mcf from msobkow.github.io/io.github.msobkow

--- src/main/java/server/markhome/mcf/v3_1/cfint/cfintsaxloader/CFIntSaxLoader.java
+++ src/main/java/server/markhome/mcf/v3_1/cfint/cfintsaxloader/CFIntSaxLoader.java
@@ -62,14 +62,14 @@ public class CFIntSaxLoader
 {
 
 	// The namespace URI of the supported schema
-	public final static String	SCHEMA_XMLNS = "https://mcf.markhome.server/mcf/xsd/v3_1/cfint-structured.xsd";
+	public final static String	SCHEMA_XMLNS = "http://mcf.markhome.server/mcf/v3_1/xsd/cfint-structured.xsd";
 
 	// The source for loading the supported schema
-	public final static String	SCHEMA_URI = "https://mcf.markhome.server/mcf/xsd/v3_1/cfint-structured.xsd";
-	public final static String	SCHEMA_ROOT_URI = "https://mcf.markhome.server/mcf/xsd/v3_1/cfint-structured.xsd";
-	public final static String CFSEC_XMLNS = "https://mcf.markhome.server/mcf/xsd/v3_1/cfsec-structured.xsd";
-	public final static String CFSEC_URI = "https://mcf.markhome.server/mcf/xsd/v3_1/cfsec-structured.xsd";
-	public final static String CFSEC_ROOT_URI = "https://mcf.markhome.server/mcf/xsd/v3_1/cfsec-structured.xsd";
+	public final static String	SCHEMA_URI = "http://mcf.markhome.server/mcf/v3_1/xsd/cfint-structured.xsd";
+	public final static String	SCHEMA_ROOT_URI = "http://mcf.markhome.server/mcf/v3_1/xsd/cfint-structured.xsd";
+	public final static String CFSEC_XMLNS = "http://mcf.markhome.server/mcf/v3_1/xsd/cfsec-structured.xsd";
+	public final static String CFSEC_URI = "http://mcf.markhome.server/mcf/v3_1/xsd/cfsec-structured.xsd";
+	public final static String CFSEC_ROOT_URI = "http://mcf.markhome.server/mcf/v3_1/xsd/cfsec-structured.xsd";
 
 
 	// The schema instance to load in to
